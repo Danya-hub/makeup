72436a3be50147adda6d8ae5220d60c742069c83
Danya <6694524@gmail.com>
2020-10-25 21:25:23 +0200

    create site makeup by mom

--- js/comments.js
+++ js/comments.js
@@ -9,7 +9,7 @@ const comments = () => {
     const blackout = document.querySelector('.blackout');
     const closeButton = document.querySelector('.bigImageWindow-closeButton');
     const bigImage = document.querySelector('.bigImageWindow--imgProcedure-img');
-    const bigImageComment = document.querySelector('.bigImageWindow--textWrapper-txt');
+    const bigImageComment = document.querySelectorAll('.bigImageWindow--textWrapper-txt>span');
     const body = document.querySelector('body');
 
     // --------- array comments ---------
@@ -19,21 +19,45 @@ const comments = () => {
     бровей + окрашивание хной. Девочки, когда
     делаете комплекс у меня, процедуры дешевле
     чем делать отдельно. Буду рада вас видеть у
-    меня на процедурах.`, `Процедура ламинирование и
+    меня на процедурах`, `Процедура ламинирование и
     ботокс ресниц для клиентки с ресницами очень
     плотным и растущими вниз. Такие реснички
-    будут вас радовать на протяжении 1-1,5 месяца.`, `Долговременная укладкабровей. Что входит в эту процедуру?
+    будут вас радовать на протяжении 1-1,5 месяца`, `Долговременная укладкабровей. Что входит в эту процедуру?
     <ul class="bigImageWindow-listProcedures">
         <li class="bigImageWindow-listProcedures-item">
             <span class="bigImageWindow-listProcedures-item-span">Укладка составами</span>
             </li>
         <li class="bigImageWindow-listProcedures-item">
             <span class="bigImageWindow-listProcedures-item-span">Коррекция бровей</span>
-            </li>
-            <li class="bigImageWindow-listProcedures-item">
+        </li>
+        <li class="bigImageWindow-listProcedures-item">
             <span class="bigImageWindow-listProcedures-item-span">Окрашивание краской</span>
         </li>
-        </ul>`, `ffff`, `dgggg`, `eee`];
+    </ul>`, `Как же я рада, что облегчаю вам
+        утренний ритуал нанесение косметики! С
+        процедурой Долговременная укладка бровей
+        достаточно утром нанести гель для бровей,
+        расчесать и готово`, `Попробуй, я думаю тебе
+        понравится! Ламинирование и ботокс ресниц.
+        На протяжении 1,5 месяца тебя будут радывать:
+    <ul class="bigImageWindow-listProcedures">
+        <li class="bigImageWindow-listProcedures-item">
+            <span class="bigImageWindow-listProcedures-item-span">Чёрные ресницы</span>
+            </li>
+        <li class="bigImageWindow-listProcedures-item">
+            <span class="bigImageWindow-listProcedures-item-span">Красивый завиток</span>
+        </li>
+        <li class="bigImageWindow-listProcedures-item">
+            <span class="bigImageWindow-listProcedures-item-span">Удлиненные</span>
+        </li>
+        <li class="bigImageWindow-listProcedures-item">
+            <span class="bigImageWindow-listProcedures-item-span">Напитанные и увлажненные изнутри</span>
+        </li>
+    </ul>`, `На фото Ваш любимый комплекс
+    Ламинирование и ботокс ресниц
+    +Долговременная укладка бровей. Эффект от
+    процедуры 1-1,5 месяца. Девочки, просыпаетесь
+    утром уже красивые`];
 
     // --------- index for image ---------
 
@@ -61,7 +85,10 @@ const comments = () => {
                     `;
 
                 bigImage.src = e.target.src;
-                bigImageComment.innerHTML = comment.find((arr, i) => i === Number(e.target.getAttribute('x')) ? arr : undefined);
+
+                for (let i = 0; i < bigImageComment.length; i++) {
+                    bigImageComment[1].innerHTML = comment.find((arr, i) => i === Number(e.target.getAttribute('x')) ? arr : undefined);
+                }
             }
         })
     }
